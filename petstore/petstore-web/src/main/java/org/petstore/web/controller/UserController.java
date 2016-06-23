eaf5496ce60b064fc11948c2063ef825edad04ce
Maxym_Rudevych <mrudevich96@gmail.com>
2016-06-23 15:09:59 +0300

    add EmailValidator, method "getUserById()" in services and daos

--- petstore/petstore-web/src/main/java/org/petstore/web/controller/UserController.java
+++ petstore/petstore-web/src/main/java/org/petstore/web/controller/UserController.java
@@ -3,20 +3,62 @@ package org.petstore.web.controller;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 
+import org.petstore.common.model.User;
 import org.petstore.ejb.service.UserService;
 
-@ManagedBean(name="userController")
+@ManagedBean(name = "userController")
 public class UserController {
 
-	private String email;
-	private String firstName;
-	private String lastName;
-	private String password;
-	
 	@EJB
 	private UserService userService;
+
+	private User user;
+
+	public UserController() {
+		user = new User();
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getUserFirstName() {
+		return user.getFirstName();
+	}
+
+	public void setUserFirstName(String firstName) {
+		user.setFirstName(firstName);
+	}
+
+	public String getUserLastName() {
+		return user.getFirstName();
+	}
+
+	public void setUserLastName(String lastName) {
+		user.setLastName(lastName);
+	}
+
+	public String getUserEmail() {
+		return user.getEmail();
+	}
+
+	public void setUserEmail(String email) {
+		user.setEmail(email);
+	}
+
+	public void setUserPassword(String password) {
+		user.setPassword(password);
+	}
+
+	public String getUserPassword() {
+		return user.getPassword();
+	}
 	
-	public boolean isFreeEmail(){
-		return true;
+	public void printUser(){
+		System.out.println(user.toString());
 	}
 }
