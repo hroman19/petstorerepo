84fdf8b7714bc6d64fc3ad85c980808726430fb0
Maxym_Rudevych <mrudevich96@gmail.com>
2016-06-27 15:57:07 +0300

    fixed problems with JsessionId, validation was fixed, added hashing for passwords

--- petstore/petstore-web/src/main/java/org/petstore/web/util/SessionUtils.java
+++ petstore/petstore-web/src/main/java/org/petstore/web/util/SessionUtils.java
@@ -12,17 +12,4 @@ public class SessionUtils {
 	public static HttpServletRequest getRequest() {
 		return (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
 	}
-
-	public static String getUserName() {
-		HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
-		return session.getAttribute("username").toString();
-	}
-
-	public static String getUserId() {
-		HttpSession session = getSession();
-		if (session != null)
-			return (String) session.getAttribute("userid");
-		else
-			return null;
-	}
 }
